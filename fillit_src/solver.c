e460437bc4a811094e80c9c7ba39c28167ad2814
Margarita Ostrovskaia <margarita.v.ostrovskaya@gmail.com>
2019-10-11 12:01:52 -0700

    Update solver.c

--- fillit_src/solver.c
+++ fillit_src/solver.c
@@ -1,30 +1,43 @@
 #include "fillit.h"
 
-void	set_def(char matrix[][LEN_FIG + 1])
+void	set_def(int count, char matrix[count][count])
 {
 	int		i;
 	int		j;
 
-	i = 0;
-	j = 0;
-	while (matrix[i][j])
+	i = -1;
+	while (++i < count)
 	{
-		matrix[i][j] = '.';
-		i++;
-		j++;
+		j = -1;
+		while (++j < count)
+			matrix[i][j] = '.';
 	}
 }
 
 //int		check_move(char matrix[][LEN_FIG + 1], t_figure *f)
 
-//void	print(char matrix[][LEN_FIG + 1])
+void	print(int count, char matrix[count][count])
+{
+	int		i;
+	int		j;
+
+	i = -1;
+	while (++i < count)
+	{
+		j = -1;
+		while (++j < count)
+			printf("%c", matrix[i][j]);
+		printf("\n");
+	}
+}
 
 int		solve(int count, t_figure *f) // Backtracking
 {
-	char	matrix[count][LEN_FIG + 1];
+	char	matrix[count * 4][count * 4];
 
 	if (!f)
 		return (0);
-	set_def(matrix);
+	set_def(count * 4, matrix);
+	print(count * 4, matrix);
 	return (1);
 }
